e5bbf93087e7a6e1296f07a2be6e9d036eaa045f
GIRARD Lucas <zbwb@girards-macbook-pro.local>
2019-02-06 04:36:30 +0100

    V.1

--- Tree2/main.cpp
+++ Tree2/main.cpp
@@ -1,61 +1,139 @@
+/**
+ Fractral arbre 6 fevrier 2019
+ LUCAS GIRARD : Universite de Sciences et Techniques de Nantes L2 informatique
+ FRAMEWORK SFML pour c++
+ */
 
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include <math.h>
 
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
+//STOQUAGE DE TROIS SET(x,y) DE POINT POUR CREE UNE BRANCHE=(LIGNE) ENTRE : PERE - FILLE GAUCHE, PERE - FILLE DROITE
+//DANS LE RESTE DU PROGRAMME
+struct branche3 {
+    branche3(sf::Vector2f par, sf::Vector2f filleG, sf::Vector2f filleD):
+    parent(par),filleGauche(filleG),filleDroite(filleD){}
+    
+    sf::Vector2f parent;
+    sf::Vector2f filleGauche;
+    sf::Vector2f filleDroite;
+};
+
+typedef std::vector<branche3> dynamicTab;
+
+void const afficheBranche(branche3 b){//AFFICHAGE D'UNE BRANCHE const CAR PAS DE MODIF DE DONNE
+    std::cout << "x parent : " << b.parent.x << std::endl;
+    std::cout << "y parent : " << b.parent.y << std::endl;
+    std::cout << "x droite : " << b.filleDroite.x << std::endl;
+    std::cout << "y droite : " << b.filleDroite.y << std::endl;
+    std::cout << "x gauche : " << b.filleGauche.x << std::endl;
+    std::cout << "y gauche : " << b.filleGauche.y << std::endl;
+    std::cout << "AAA" << std::endl;
+}
+
+void theTree(dynamicTab & v, const sf::Vector2f start, const float lon, const float startingAngle, float alpha, float ecart, int pixelStop, const int option){
+    
+    //SI LA LONGUEUR DE LA BRANCHE A CALCULER (lon) EST PLUS ELEVER QUE pixelStop
+    if(lon > pixelStop){
+        
+        //SET DES ANGLES FILLE DROITE ET GAUCHE AUTOUR DU STARTING POINT (±ecart)
+        float angleFilleG = startingAngle + ecart;
+        float angleFilleD = startingAngle - ecart;
+        
+        //ON OBTIENS LES COORDONNES DES POINTS FILLE GAUCHE ET DROITE GRACE AU COSE ET AU SIN DES ANGLES CALCULER PRECEDEMENT
+        sf::Vector2f filleG(start.x + cos(angleFilleG) * lon, start.y + sin(angleFilleG) * lon);
+        sf::Vector2f filleD(start.x + cos(angleFilleD) * lon, start.y + sin(angleFilleD) * lon);
+        //ON CREE LA BRANCHE CORRESPONDANT AVEC LE PERE ET LES FILLES
+        branche3 * b = new branche3(start,filleG,filleD);
+        //ON AJOUTE LA BRANCHE AU VECTOR DE BRANCHE
+        v.push_back(*b);//A GARDER COMME CA MOINS DE FUITE MEMOIRE QUE v.push_back(*new branche3(start,filleG,filleD); QUAND DELETE MANUELLEMENT
+        delete b;//ON LIBERE LA MEMOIRE
+        if(option == 1){//arbre libre
+            //RECURSION ON MODIFIE LE STARTING POINT POUR AVOIR UN PETIT ECART AVEC PAR RAPPORT A LA BRANCHE PRECEDENTE
+            theTree(v,filleG, lon/1.6, startingAngle+alpha, alpha, ecart, pixelStop, option);
+            theTree(v,filleD, lon/1.6, startingAngle-alpha, alpha, ecart, pixelStop, option);
+        }else if(option == 2){//arbre proportionnelle (plus parfait)
+            theTree(v,filleG, lon/1.6, startingAngle+ecart, alpha, ecart, pixelStop, option);
+            theTree(v,filleD, lon/1.6, startingAngle-ecart, alpha, ecart, pixelStop, option);
+        }else if(option == 3){
+            
+        }else{//par default
+            theTree(v,filleG, lon/1.6, startingAngle+ecart, alpha, ecart, pixelStop, option);
+            theTree(v,filleD, lon/1.6, startingAngle-ecart, alpha, ecart, pixelStop, option);
+        }
+    }
+}
+
+sf::VertexArray fillLignes(sf::Vector2f startingPoint, const float longeur, float alpha, float beta, int pixelStop, int option){
+    dynamicTab tabBranche;//UTILISATION DU TYPE EXPLICITE PLUS HAUT (vector<branche3>)
+    //APPELLE DE LA RECURSIVE QUI CALCULES LES BRANCHES (TROIS POINT(x,y) PARENT, FILLE GAUCHE, FILLE DROITE)
+    //REMPLIS tabBranche
+    //-M_PI/2 CORRESPOND A L'ORIENTATION DE LA STRUCTURE ICI VERS LE HAUT
+    theTree(tabBranche, startingPoint, longeur, -M_PI/2, alpha, beta, pixelStop, option);
+    //VECTOR DE LIGNES (SET(x,y) DE POINTs DEUX PAR DEUX), DE TAILLE 4*tabBranche
+    //CAR UNE BRANCHE = 2 LIGNES (PERE - FILLE GAUCHE) (PERE - FILLE DROITE) = 4 SET DE POINT
+    sf::VertexArray ligne(sf::Lines, 4*tabBranche.size());
+    //CURRENT VAS AUGMENTER 4 FOIS A CHAQUE TOUR POUR SUIVRE LE CONTENU DE line DE TAILLE 4*tabBranche
+    int current=0;
+    for(int i = 0 ; i < tabBranche.size(); i++){
+        //ON ENREGISTRE LES LIGNES (SET(x,y) DE POINTS) COMME EXPLIQUER PLUS HAUT
+        ligne[current++].position = tabBranche[i].parent;
+        ligne[current++].position = tabBranche[i].filleGauche;
+        ligne[current++].position = tabBranche[i].parent;
+        ligne[current++].position = tabBranche[i].filleDroite;
+    }
+    return ligne;
+}
+
 int main(int, char const**)
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
+    sf::RenderWindow window(sf::VideoMode(1800, 1800), "SFML window", sf::Style::None);
+    
+    //VARIABLES
+    sf::Vector2f startingPoint(600.0,600.0);//POINT DE DEPART DE LA RECURSIVE
+    const float longueur = 200;  //LONGUEUR DE LA PREMIERE BRANCHE EN PX LES BRANCHE
+    //SONT DE LONGUEUR longueur/1.6 PAR RAPPORT A LA BRANCHE PRECEDENTE
+    float alpha = M_PI/4;       //ANGLE ECART ENTRE BRANCHE PERE ET BRANCHE FILLE
+    //(POUR QUE LES DEUX BRANCHES SOIT A EQUIDISTANSE DE LA BRANCHE PERE alpha = PI/4)
+    float beta = M_PI/4;        //ANGLE ECART ENTRE LES DEUX BRANCHE
+    bool autoMov=false;         //L'ECART ENTRE LES BRANCHE ET LA BRANCHE PERE AUGMENTE A CHAQUE FRAME TOUT SEUL
+    bool autoEcart=false;       //L'ECART ENTRE LES DEUX BRANCHE AUGMENTE A CHAQUE FRAME TOUT SEUL
+    float augMovAuto = 0.01;    //SI autoMov = true alpha+=augMovAuto
+    float augEcartAuto = 0.01;  //SI autoEcart = true beta+=augEcartAuto
+    bool followMouse=false;     //ACTIVE LE SUIVIT DE LA SOURIS
+    int pixelStop = 2;          //LONGUEUR MINIMAL D'UNE BRANCHE A PARTIR DE LAQUEL LE PROGRAMME
+    //NE CHERCHE PLUS LES BRANCHE RECURSIVEMENT(CONDITION D'ARRET) EN PIXEL
+    sf::Vertex startLigne[2] = {sf::Vertex(sf::Vector2f(startingPoint.x,startingPoint.y)),sf::Vertex(sf::Vector2f(startingPoint.x,startingPoint.y+longueur))};
+    //LIGNE DECORATIVE DE DEPART ^
+    int option = 1;             //OPTION A APPLIQUER A LA BOUCLE RECURSIVE POUR DIFFERENTS EFFETS
+    //LIGNE STOQUE LES LIGNE (1 LIGNE = DEUX SET(x,y) DE POINT SUCCESSIF (DEUX PAR DEUX PAS PLUS))
+    //ON REMPLIS ligne GRACE A LA FONCTION PRINCIPALE
+    sf::VertexArray ligne = *new sf::VertexArray(fillLignes(startingPoint, longueur, alpha, beta, pixelStop, 1));
+    
+    
+    //CREATION DU TEST POUR AFFICHER ALPHA ET BETA DANS LA FENETRE
     sf::Font font;
     if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
         return EXIT_FAILURE;
     }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
+    //TAILLE
+    unsigned int textSize=50;
+    sf::Text text("", font, textSize);
+    //TEXTE BLANC POUR VOIR SUR FOND NOIR
+    text.setFillColor(sf::Color::White);
+    
+    sf::Texture img;
+    if(!img.loadFromFile(resourcePath() + "fond.png")){
         return EXIT_FAILURE;
     }
-
-    // Play the music
-    music.play();
-
+    sf::Sprite fond(img);
+    
+    window.setFramerateLimit(60);
     // Start the game loop
     while (window.isOpen())
     {
@@ -67,25 +145,114 @@ int main(int, char const**)
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-
+            
             // Escape pressed: exit
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
             }
+            
+            //OPTIONS
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) {
+                //ON/OFF DE L'AUGMENTATION AUTOMATIQUE (+1 par frame) DE L'ECART ALPHA
+                //NE FONCTIONNE PAS AVEC -E (alpha fixe)
+                autoMov=!autoMov;
+                augMovAuto = 0.01;
+                followMouse=false;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down) {
+                //ON/OFF DE L'AUGMENTATION AUTOMATIQUE (+1 par frame) DE L'ECART ALPHA
+                //NE FONCTIONNE PAS AVEC -E (alpha fixe)
+                //SENS CONTRAIRE A UP
+                autoMov=!autoMov;
+                augMovAuto = -0.01;
+                followMouse=false;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right) {
+                //ON/OFF DU L'AUGMENTATION AUTOMATIQUE (+1 par frame) DE L'ECART ENTRE DEUX BRANCHE
+                autoEcart=!autoEcart;
+                augEcartAuto = 0.01;
+                followMouse=false;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left) {
+                //ON/OFF DU L'AUGMENTATION AUTOMATIQUE (+1 par frame) DE L'ECART ENTRE DEUX BRANCHE
+                //SENS CONTRAIRE A RIGHT
+                autoEcart=!autoEcart;
+                augEcartAuto = -0.01;
+                followMouse=false;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A) {
+                //CONTROLE ALPHA ET ECART GRACE A LA SOURIS X ET Y
+                followMouse=!followMouse;
+                autoMov=false;
+                autoEcart=false;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D) {//NORMAL
+                option=1;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::E) {//PROPORTIONELLE
+                option=2;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::F) {//NULL
+                option=3;
+            }
+            
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::G) {
+                //REDEFINITION DE LA LONGUEURE REQUISE POUR CONTINUER LA RECURSIVITE
+                //JOLIE EFFET DE RECONSTRUCTION GRACE AU SI(>2):pixelStop++ DANS LE DRAW
+                pixelStop=100;
+            }
+            
         }
-
+        
+        //AFFICHE ALPHA ET BETA SUR L'ECRAN
+        text.setString(std::to_string(alpha) + " : " + std::to_string(beta));
         // Clear screen
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
+        
+        
+        if(autoMov){//AUGMENTATION DE ALPHA AUTO
+            alpha+=augMovAuto;
+        }
+        if(autoEcart){//AUGMENTATION DE BETA AUTO
+            beta+=augEcartAuto;
+        }
+        if(followMouse){
+            //ALPHA(X) ET BETA(Y) EVOLUE EN FONCTION DE LA SOURIS
+            sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+            alpha = 0.001*localPosition.y;
+            beta = 0.001*localPosition.x;
+        }
+        for(int i =0; i<ligne.getVertexCount(); i++){
+            //DESTRUCTION DES LIGNES AVANT DE LES RECALCULER AVEC LES EVENTUELLES NOUVEAU PARAMETRES
+            //EVITE LES FUITES MEMOIRE !IMPORTANT!
+            ligne[i].~Vertex();
+        }
+        
+        if(pixelStop>2){
+            //LE NOMBRE DE PIXEL NECESSAIRE A FAIRE CALCULER UNE BRANCHE DIMINUE : LES BRANCHE REAPPARAISSE AU FUR ET A MESURE
+            //A COMBINER AVEC -G
+            pixelStop-=1;
+        }
+        
+        ligne.clear();
+        //CREATION DES LIGNES AVEC LES PARAMETRE MIS A JOURS
+        ligne = fillLignes(startingPoint, longueur, alpha, beta, pixelStop, option);
+        //DRAW LA PREMIERE LIGNES
+        window.draw(fond);
+        window.draw(startLigne, 2, sf::Lines);
         window.draw(text);
-
-        // Update the window
+        //DRAW LES LIGNE
+        window.draw(ligne);
         window.display();
     }
-
+    
     return EXIT_SUCCESS;
 }
